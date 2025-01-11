9c347f7250955a3c71da94b0659bd51688abe38d
Kurbz <ljd622@gmail.com>
2025-01-10 18:20:02 -0600

    remove out

--- out/build/Clang 14.0.0 arm64-apple-darwin22.3.0/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/landondevendorf/Documents/GitHub/CVulkanVoxelEngine
-# Build directory: /Users/landondevendorf/Documents/GitHub/CVulkanVoxelEngine/out/build/Clang 14.0.0 arm64-apple-darwin22.3.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/cglm")
